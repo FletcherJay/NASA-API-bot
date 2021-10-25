f618cbfcfabee2a43834142e5f1a761eba29f768
Samuel Johnson <samuel.f.johnson96@gmail.com>
2021-10-25 10:54:40 -1000

    Add auto date for api

---
+++ app.js
@@ -0,0 +1,73 @@
+let API_KEY = 'qnijM37ihRFu5YgP4NdXg2fn3xUIqVUqxcy5ZxZy'
+
+
+
+let endDate = new Date()
+let currentYear = endDate.getFullYear()
+let currentMonth = endDate.getMonth()
+let currentDay = endDate.getDate()
+
+let startDate = '2021/05/05'
+
+//i could have input be mm/dd/yyyy, then just use a .split {/} then just reverse that new array, that way they can chose
+//make todays date to be easy, once everything is finished let this be chosen
+//i think it would be cool if it could tweet a user
+let API_URL = `https://api.nasa.gov/DONKI/notifications?startDate=${startDate}&endDate=${endDate}&type=all&api_key=${API_KEY}`
+
+let selectedEventIndex = ''
+
+getNasaInfo().then(event => {
+    selectedEventIndex = event.length - event.length
+    displaySelectedEvent(event)
+
+});
+
+const currentMessageElement = document.querySelector('[data-message-body]')
+const currentMessageDateElement = document.querySelector('[data-message-Time]')
+const currentEventTypeElement = document.querySelector('[data-event-Type]')
+
+function displaySelectedEvent(event) {
+    const selectedEvent = event[selectedEventIndex]
+    currentMessageElement.innerText = selectedEvent.eventMessage
+    currentMessageDateElement.innerText = selectedEvent.eventDate
+    currentEventTypeElement.innerText = selectedEvent.eventType
+
+}
+function getNasaInfo() {
+    return fetch(API_URL)
+        .then(res => res.json())
+        .then(data => {
+            const {
+                ...spaceEvent
+            } = data
+            return Object.entries(spaceEvent).map(([event, data]) => {
+                return {
+                    event: event,
+                    eventType: data.messageType,
+                    eventDate: data.messageIssueTime,
+                    eventURL: data.messageURL,
+                    eventMessage: data.messageBody
+
+                }
+            })
+
+        })
+}
+
+//add function to be able to have entire list between specified time frame unter a clickable event
+//where each list item has their own expandable and collapsable breakeaway 
+
+//have api have choseable dates
+
+//have default date be tomorrows date, and 1 week ago
+
+//have background picture be based on the report ex. cme, have a cme in the background on that part of the body for said list item
+
+//have collabsible div chose by date, or event type
+//if event type still order by closest date to earliest date, top to bottom
+//need to have a function that goes through all api ul
+//create a new div for each event typ and when clicked it shows the message body
+
+
+
+
